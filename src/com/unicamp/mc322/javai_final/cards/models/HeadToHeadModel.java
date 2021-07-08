8264f6b6a9467734587d25add16bc97d7352edd4
rianradeck <rianradeck3@gmail.com>
2021-07-08 18:17:48 -0300

    Implentei IA em DoubledValue e HeadToHead

--- src/com/unicamp/mc322/javai_final/cards/models/HeadToHeadModel.java
+++ src/com/unicamp/mc322/javai_final/cards/models/HeadToHeadModel.java
@@ -20,15 +20,34 @@ public class HeadToHeadModel extends SpellCardModel {
 		g.addListener(new InputListener() {
 			@Override
 			public void onInput(String input) {
-				String[] s = input.split(" ");
-				int[] indices = new int[s.length];
-				for (int i = 0; i < indices.length; i++)
-					indices[i] = Integer.parseInt(s[i]);
-
-				Card attacking = manager.getCurrentPlayer().getFieldCards()[indices[0]];
-				Card defending = manager.getOpponentPlayer().getFieldCards()[indices[1]];
-
-				manager.doCombat(attacking, defending);
+				if(manager.getCurrentPlayer().isAI()) {
+					for(int i = 0;i < manager.getCurrentPlayer().getFieldCards().length;i++) {
+						for(int j = 0;j < manager.getOpponentPlayer().getFieldCards().length;j++) {
+							if(manager.getCurrentPlayer().getFieldCards()[i] != null && manager.getOpponentPlayer().getFieldCards()[j] != null) {
+								manager.doCombat(manager.getCurrentPlayer().getFieldCards()[i], manager.getOpponentPlayer().getFieldCards()[j]);
+								return;
+							}
+						}
+					}
+					
+				} else {
+					
+					String[] s = input.split(" ");
+					int[] indices = new int[s.length];
+					try {
+						for (int i = 0; i < indices.length; i++)
+							indices[i] = Integer.parseInt(s[i]);
+					} catch (NumberFormatException e) {
+						System.err.println("Invalid input");
+						onSummon(card);
+						return;
+					}
+	
+					Card attacking = manager.getCurrentPlayer().getFieldCards()[indices[0]];
+					Card defending = manager.getOpponentPlayer().getFieldCards()[indices[1]];
+	
+					manager.doCombat(attacking, defending);
+				}
 			}
 		});
 	}
