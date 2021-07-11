0ae065b686512d600402f7d8d97f880f40c4459e
Chainim <igorbritoandrade@gmail.com>
2021-07-11 18:43:33 -0300

    Refactoring

--- src/com/unicamp/mc322/javai_final/cards/models/HeadToHeadModel.java
+++ src/com/unicamp/mc322/javai_final/cards/models/HeadToHeadModel.java
@@ -5,6 +5,7 @@ import com.unicamp.mc322.javai_final.cards.SpellCardModel;
 import com.unicamp.mc322.javai_final.gamestate.GameStateManager;
 import com.unicamp.mc322.javai_final.gamestate.InputListener;
 import com.unicamp.mc322.javai_final.gamestate.SummonState;
+import com.unicamp.mc322.javai_final.util.InputUtils;
 
 public class HeadToHeadModel extends SpellCardModel {
 
@@ -43,28 +44,12 @@ public class HeadToHeadModel extends SpellCardModel {
 			@Override
 			public boolean onInput(String input) {
 				
-				if(input.equals("done")) {
-					System.err.println("Clique em alguma carta do campo para atacar");
+				int index = InputUtils.expectCardOnCurrentPlayerField(input);
+				if(index == -1)
 					return false;
-				}
-				String[] s = input.split(" ");
-				if(Integer.parseInt(s[2]) != manager.getCurrentPlayerIndex()) {
-					System.err.println("Selecione somente suas coisas");
-					return false;
-				} else if(s[0].equals("hand")) {
-					System.err.println("Selecione alguma carta do campo");
-					return false;
-				} else {
-					int index = Integer.parseInt(s[1]);
-					if(manager.getCurrentPlayer().getFieldCards()[index] == null) {
-						System.err.println("Campo vazio");
-						return false;
-					} else {
-						g.attacking1v1 = manager.getCurrentPlayer().getFieldCards()[index];
-						System.out.println("ADICIONEI O ATACANTE NO STATE");
-						return true;
-					}
-				}
+				g.attacking1v1 = manager.getCurrentPlayer().getFieldCards()[index];
+				return true;
+				
 			}
 		});
 		
@@ -72,28 +57,12 @@ public class HeadToHeadModel extends SpellCardModel {
 			@Override
 			public boolean onInput(String input) {
 				
-				if(input.equals("done")) {
-					System.err.println("Clique em alguma carta do campo inimigo para defender");
+				int index = InputUtils.expectCardOnOpponentPlayerField(input);
+				if(index == -1)
 					return false;
-				}
-				String[] s = input.split(" ");
-				if(Integer.parseInt(s[2]) != manager.getOpponentPlayerIndex()) {
-					System.err.println("Selecione somente suas coisas");
-					return false;
-				} else if(s[0].equals("hand")) {
-					System.err.println("Selecione alguma carta do campo");
-					return false;
-				} else {
-					int index = Integer.parseInt(s[1]);
-					if(manager.getOpponentPlayer().getFieldCards()[index] == null) {
-						System.err.println("Campo vazio");
-						return false;
-					} else {
-						g.defending1v1 = manager.getOpponentPlayer().getFieldCards()[index];
-						System.out.println("ADICIONEI O DEFENSOR NO STATE");
-						return true;
-					}
-				}
+				g.defending1v1 = manager.getCurrentPlayer().getFieldCards()[index];
+				return true;
+				
 			}		
 		});
 	}
--- src/com/unicamp/mc322/javai_final/util/InputUtils.java
+++ src/com/unicamp/mc322/javai_final/util/InputUtils.java
@@ -26,4 +26,28 @@ public class InputUtils {
 			}
 		}
 	}
+	
+	public static int expectCardOnOpponentPlayerField(String input) {
+		GameStateManager manager = GameStateManager.getInstance();
+		if(input.equals("done")) {
+			System.err.println("Clique em alguma carta do campo inimigo para defender");
+			return -1;
+		}
+		String[] s = input.split(" ");
+		if(Integer.parseInt(s[2]) != manager.getOpponentPlayerIndex()) {
+			System.err.println("Selecione somente suas coisas");
+			return -1;
+		} else if(s[0].equals("hand")) {
+			System.err.println("Selecione alguma carta do campo");
+			return -1;
+		} else {
+			int index = Integer.parseInt(s[1]);
+			if(manager.getOpponentPlayer().getFieldCards()[index] == null) {
+				System.err.println("Campo vazio");
+				return -1;
+			} else {
+				return index;
+			}
+		}
+	}
 }
